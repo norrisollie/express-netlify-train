47c34a5c9a7112d946b3617ec47819afb3fb0472
Ollie Norris <norrisollie@icloud.com>
2023-08-17 09:09:29 +0100

    initial commit

--- src/api.js
+++ src/api.js
@@ -1,16 +1,48 @@
 const express = require("express")
 const serverless = require("serverless-http")
+const options = require("./helpers/headers")
+const needle = require("needle")
+require("dotenv").config()
+
+// env variables
 
 const app = express()
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-	return res.json({
-		hello: "hi",
-	})
+const API_BASE_URL = process.env.API_BASE_URL
+
+router.get("/search/:code", async (req, res) => {
+	try {
+		// get CRS code from station query in URL
+		const stationCode = req.params.code
+
+		// construct URL to make request
+		const apiRes = await needle(
+			`${API_BASE_URL}/search/${stationCode}`,
+			options
+		)
+		const data = apiRes.body
+		res.status(200).json(data)
+	} catch (error) {
+		res.status(500).json({error})
+	}
 })
 
-app.use("/.netlify/functions/api", router)
+router.get("/search/:service", async (req, res) => {
+	try {
+		// get CRS code from station query in URL
+		const stationCode = req.params.code
+
+		// construct URL to make request
+		const apiRes = await needle(`${API_BASE_URL}/search/${service}`, options)
+		const data = apiRes.body
+		res.status(200).json(data)
+	} catch (error) {
+		res.status(500).json({error})
+	}
+})
+
+app.use("/api", router)
 
 module.exports.handler = serverless(app)
