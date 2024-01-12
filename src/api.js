601535ecbf84582c577ea25587fcf6fcf606ad35
Ollie Norris <norrisollie@icloud.com>
2024-01-12 18:39:34 +0000

    added new route to get stations from X - to - Y

--- src/api.js
+++ src/api.js
@@ -12,6 +12,7 @@ const router = express.Router()
 
 const API_BASE_URL = process.env.API_BASE_URL
 
+// route for station timetable
 router.get("/search/:code", async (req, res) => {
 	try {
 		// get CRS code from station query in URL
@@ -20,6 +21,25 @@ router.get("/search/:code", async (req, res) => {
 		// construct URL to make request
 		const apiRes = await needle(
 			`${API_BASE_URL}/search/${stationCode}`,
+			options
+		)
+		const data = apiRes.body
+		res.status(200).json(data)
+	} catch (error) {
+		res.status(500).json({error})
+	}
+})
+
+// route for station journeys - from station - to station
+router.get("/search/:from/to/:to", async (req, res) => {
+	try {
+		// get CRS code from station query in URL
+		const stationCodeFrom = req.params.from
+		const stationCodeTo = req.params.to
+
+		// construct URL to make request
+		const apiRes = await needle(
+			`${API_BASE_URL}/search/${stationCodeFrom}/to/${stationCodeTo}`,
 			options
 		)
 		const data = apiRes.body
@@ -47,10 +67,6 @@ router.get("/service/:serviceid/:year/:month/:date", async (req, res) => {
 	} catch (error) {
 		res.status(500).json({error})
 	}
-})
-
-router.get("/hello", async (req, res) => {
-	res.status(200).json({hello: "hi"})
 })
 
 app.use("/api/", router)
